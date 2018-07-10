0a7bffe8b656f62c34999fcbf89008898dae9330
YarotskiyAndrey <yrtsky@gmail.com>
2018-07-10 15:34:35 +0300

    Script with mistakes

---
+++ task3.py
@@ -0,0 +1,9 @@
+import sys , random
+def  compute(n):
+    i = 0; s = 0
+    while i  <= n:
+        s +=  random.random ()
+        i += 1
+    return s/n
+n = sys.argv [1]
+print  ’average  of %d random  numbers  is %g" % (n, compute(n))
